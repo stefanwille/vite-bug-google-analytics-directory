b182e3285ebda133b02835546d7614e2ee010a19
Stefan Wille <github@stefanwille.com>
2022-03-01 18:00:34 +0100

    Add MyComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import MyComponent from "./GoogleAnalyticsABC/MyComponent";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      Hello
+      <MyComponent />
+    </div>
+  );
 }
 
 export default App;
